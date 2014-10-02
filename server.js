39b879dd3274839df3ce74a331611b08554fe71f
Brian J Dixon <bjdixon@gmail.com>
2014-10-01 20:12:24 -0400

    Added express listening to port 8080 and restbus embedded on 3000

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// setup
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
 // start the restbus server 
 // (serves the nextbus xml api as json)
 var restbus = require('restbus');
 
-restbus.listen('3000', function() {
-    console.log('restbus is listening on port 3000');
-});
+// configuration
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/json'}));
 
+// App listens on port 8080, restbus listens on port 3000
+app.listen(8080, function() {
+    console.log('app listening on port 8080');
+    restbus.listen(3000, function() {
+        console.log('restbus is listening on port 3000');
+    });
+});
