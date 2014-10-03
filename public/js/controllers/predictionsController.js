7398fcbbd478b21b6712955ed42fcd360eac6505
Brian J Dixon <bjdixon@gmail.com>
2014-10-03 00:23:28 -0400

    Initial display outputs a list of agencies with the option to order by region or title

--- public/js/controllers/predictionsController.js
+++ public/js/controllers/predictionsController.js
@@ -3,11 +3,12 @@
 predictionsApp.controller('PredictionsController',
     function PredictionsController($scope, $http) {
         $scope.message = 'hello world';
+        $scope.agencies = {};
 
-        $http.get('http://localhost:3000/agencies/') 
+        $http.get('http://localhost:3000/agencies') 
             .success(function(data) { 
                 $scope.agencies = data; 
-                console.log(data); 
+                console.log($scope.agencies); 
             })
             .error(function(data) {
                 console.log('ERROR: ' + data);
