a949d7da0dd2398b3847237a2b099e0131bbab54
Brian J Dixon <bjdixon@gmail.com>
2014-10-02 23:58:45 -0400

    Added README, LICENSE and a test route

--- public/js/controllers/predictionsController.js
+++ public/js/controllers/predictionsController.js
@@ -1,7 +1,16 @@
 'use strict';
 
 predictionsApp.controller('PredictionsController',
-    function PredictionsController($scope) {
+    function PredictionsController($scope, $http) {
         $scope.message = 'hello world';
+
+        $http.get('http://localhost:3000/agencies/') 
+            .success(function(data) { 
+                $scope.agencies = data; 
+                console.log(data); 
+            })
+            .error(function(data) {
+                console.log('ERROR: ' + data);
+            });
     }
 );
