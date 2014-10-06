d6b24d094a4f8f21e0d608a0260c1c9c1da28a6a
Brian J Dixon <bjdixon@gmail.com>
2014-10-05 23:30:02 -0400

    Added routing and views for stops per route and arrivals predictions

--- public/js/controllers/predictionsController.js
+++ public/js/controllers/predictionsController.js
@@ -7,7 +7,6 @@ predictionsApp.controller('agenciesController',
         $http.get('http://localhost:3000/agencies') 
             .success(function(data) { 
                 $scope.agencies = data; 
-                console.log($scope.agencies); 
             })
             .error(function(data) {
                 console.log('ERROR: ' + data);
@@ -18,11 +17,10 @@ predictionsApp.controller('agenciesController',
 predictionsApp.controller('routesController',
     function routesController($scope, $http, $routeParams) {
         $scope.routes = {};
-        var agency = $routeParams.agency_id;
+        $scope.agency_id = $routeParams.agency_id;
 
-        $http.get('http://localhost:3000/agencies/' + agency + '/routes')
+        $http.get('http://localhost:3000/agencies/' + $scope.agency_id + '/routes')
             .success(function(data) {
-                $scope.agency_id = agency;
                 $scope.routes = data;
             })
             .error(function(data) {
@@ -31,3 +29,35 @@ predictionsApp.controller('routesController',
     }
 );
 
+predictionsApp.controller('stopsController',
+    function stopsController($scope, $http, $routeParams) {
+        $scope.stops = {};
+        $scope.agency_id = $routeParams.agency_id;
+        $scope.route_id = $routeParams.route_id;
+
+        $http.get('http://localhost:3000/agencies/' + $scope.agency_id + '/routes/' + $scope.route_id)
+            .success(function(data) {
+                $scope.stops = data.stops;
+            })
+            .error(function(data) {
+                console.log('ERROR: ' + data);
+            });
+    }
+);
+
+predictionsApp.controller('predictionsController',
+    function predictionsController($scope, $http, $routeParams) {
+        $scope.predictions = {};
+        $scope.agency_id = $routeParams.agency_id;
+        $scope.route_id = $routeParams.route_id;
+        $scope.stop_id = $routeParams.stop_id;
+
+        $http.get('http://localhost:3000/agencies/' + $scope.agency_id + '/routes/' + $scope.route_id + '/stops/' + $scope.stop_id + '/predictions')
+            .success(function(data) {
+                $scope.predictions = data[0].values;
+            })
+            .error(function(data) {
+                console.log('ERROR: ' + data);
+            });
+    }
+);
