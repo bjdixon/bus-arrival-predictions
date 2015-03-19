4ec97293e105af3e434996336ed8d475f9b58056
Brian J Dixon <bjdixon@gmail.com>
2015-03-18 20:26:20 -0400

    edited server.js for deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/json'}));
 
 // App listens on port 8080, restbus listens on port 3000
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('app listening on port 8080');
     restbus.listen(3000, function() {
         console.log('restbus is listening on port 3000');
