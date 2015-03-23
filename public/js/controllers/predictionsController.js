9d380d176f4cced588208469dfdafb86a68461a4
Brian J Dixon <bjdixon@gmail.com>
2015-03-23 15:41:37 -0400

    moving fetch and format logic from controller to service

--- public/js/controllers/predictionsController.js
+++ public/js/controllers/predictionsController.js
@@ -9,100 +9,63 @@ predictionsApp.controller('menuController',
 );
 
 predictionsApp.controller('agenciesController',
-    function agenciesController($scope, $http, appConstants) {
-        var url = appConstants.api_url + '/agencies';
-        $scope.agencies = {};
+    function agenciesController($scope, $http, fetchRestbusDataFactory) {
         $scope.data_loading = true;
-
-        $http.get(url) 
-            .success(function(data) { 
-                $scope.agencies = data; 
+        fetchRestbusDataFactory.get_agencies()
+            .then(function(data) {
+                $scope.agencies = data;
                 $scope.data_loading = false;
-            })
-            .error(function(data) {
-                console.log('ERROR: ' + data);
             });
     }
 );
 
 predictionsApp.controller('routesController',
-    function routesController($scope, $http, $routeParams, appConstants) {
-        var url;
-        $scope.routes = {};
-        $scope.agency_id = $routeParams.agency_id;
+    function routesController($scope, $http, $routeParams, fetchRestbusDataFactory) {
         $scope.data_loading = true;
-        url = appConstants.api_url + '/agencies/' + $scope.agency_id + '/routes';
-
-        $http.get(url)
-            .success(function(data) {
+        $scope.agency_id = $routeParams.agency_id;
+        fetchRestbusDataFactory.get_routes($routeParams.agency_id)
+            .then(function(data) {
                 $scope.routes = data;
-                // TTC specific formatting for routes
-                if ($scope.agency_id === 'ttc') {
-                    angular.forEach($scope.routes, function(value, key) {
-                        value.title = value.title.split('-')[1];
-                    });
-                }
                 $scope.data_loading = false;
-            })
-            .error(function(data) {
-                console.log('ERROR: ' + data);
             });
     }
 );
 
 predictionsApp.controller('stopsController',
-    function stopsController($scope, $http, $routeParams, appConstants) {
-        var url;
-        $scope.stops = {};
+    function stopsController($scope, $http, $routeParams, fetchRestbusDataFactory) {
         $scope.agency_id = $routeParams.agency_id;
         $scope.route_id = $routeParams.route_id;
         $scope.route_title = '';
         $scope.data_loading = true;
-        url = appConstants.api_url + '/agencies/' + $scope.agency_id + '/routes/' + $scope.route_id;
-
-        $http.get(url)
-            .success(function(data) {
+        fetchRestbusDataFactory.get_stops($routeParams.agency_id, $routeParams.route_id)
+            .then(function(data) {
                 $scope.stops = data.stops;
                 $scope.route_title = data.title;
                 $scope.data_loading = false;
-            })
-            .error(function(data) {
-                console.log('ERROR: ' + data);
             });
     }
 );
 
 predictionsApp.controller('predictionsController',
-    function predictionsController($scope, $http, $routeParams, $cookies, $cookieStore, $location, appConstants) {
-        var url;
-        $scope.predictions = {};
+    function predictionsController($scope, $http, $routeParams, fetchRestbusDataFactory) {
         $scope.agency_id = $routeParams.agency_id;
         $scope.route_id = $routeParams.route_id;
         $scope.route_title = '';
         $scope.stop_id = $routeParams.stop_id;
         $scope.stop_title = '';
         $scope.data_loading = true;
-        url = appConstants.api_url + '/agencies/' + $scope.agency_id + '/routes/' + $scope.route_id + '/stops/' + $scope.stop_id + '/predictions';
-
-        $http.get(url)
-            .success(function(data) {
-                $scope.predictions_available = false;
-                $scope.predictions = (data[0].values != undefined) ? data[0].values : [];
-                if ($scope.predictions.length > 0) {
+        fetchRestbusDataFactory.get_predictions($routeParams.agency_id, $routeParams.route_id, $routeParams.stop_id)
+            .then(function(data) {
+                if (data !== undefined) {
+                    $scope.predictions = data.predictions;
                     $scope.predictions_available = true;
+                    $scope.route_title = data.route_title;
+                    $scope.stop_title = data.stop_title;
+                    $scope.data_loading = false;
+                } else {
+                    $scope.predictions_available = false;
+                    $scope.data_loading = false;
                 }
-                $scope.route_title = data[0].route.title;
-                $scope.stop_title = data[0].stop.title;
-                $scope.data_loading = false;
-                $cookieStore.put("last_search", {
-                    route: $scope.route_title,
-                    stop: $scope.stop_title,
-                    url: $location.path(),
-                    api_url: url
-                });
-            })
-            .error(function(data) {
-                console.log('ERROR: ' + data);
             });
     }
 );
